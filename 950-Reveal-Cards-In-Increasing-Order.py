4cab9777169e402e186491d720179274e058fd93
Rewaa Alaa <110832512+rewaaalaa7@users.noreply.github.com>
2024-04-10 03:27:25 +0200

    [950] [Time Beats: 83.79%] [Memory Beats: 9.42%] - LeetPush

--- 950-Reveal-Cards-In-Increasing-Order.py
+++ 950-Reveal-Cards-In-Increasing-Order.py
@@ -1,5 +1,10 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        '''
+        1. sort dec
+        2. pop right side card from deque then put it to the front ot them
+        3. pop top card from deque, put into front of deque
+        '''
         deck.sort(reverse=True)
         queue = collections.deque()
         for card in deck:
